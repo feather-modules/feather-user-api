9eebf0383cfd242ec598168e9fa2673d717bc99d
GErP83 <gurrka@gmail.com>
2024-04-09 13:25:12 +0200

    update package

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/feather-framework/feather-openapi-kit", .upToNextMinor(from: "0.9.0")),
         .package(url: "https://github.com/feather-framework/feather-api-kit", .upToNextMinor(from: "0.1.0")),
-        .package(url: "https://github.com/feather-modules/feather-system-api", .upToNextMinor(from: "0.1.0")),
+        .package(url: "https://github.com/feather-modules/feather-system-api", .upToNextMinor(from: "0.2.0")),
         .package(url: "https://github.com/jpsim/Yams", from: "5.0.0"),
     ],
     targets: [

71546d5f901f8c4d00e369b6deef02ee8e9946d3
GErP83 <gurrka@gmail.com>
2024-10-18 16:44:12 +0200

    test sh with curl

--- scripts/check-broken-symlinks.sh
+++ scripts/check-broken-symlinks.sh
@@ -5,20 +5,24 @@ log() { printf -- "** %s\n" "$*" >&2; }
 error() { printf -- "** ERROR: %s\n" "$*" >&2; }
 fatal() { error "$@"; exit 1; }
 
-CURRENT_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-REPO_ROOT="$(git -C "${CURRENT_SCRIPT_DIR}" rev-parse --show-toplevel)"
+REPO_ROOT="$(git -C "$PWD" rev-parse --show-toplevel)"
 
 log "Checking for broken symlinks..."
 NUM_BROKEN_SYMLINKS=0
+
+# Loop through each file listed by git
 while read -r -d '' file; do
-  if ! test -e "${REPO_ROOT}/${file}"; then
-    error "Broken symlink: ${file}"
-    ((NUM_BROKEN_SYMLINKS++))
-  fi
+    # Check if the symlink is broken (i.e., target does not exist)
+    if ! test -e "${REPO_ROOT}/${file}"; then
+        log "Broken symlink: ${file}"
+        ((NUM_BROKEN_SYMLINKS++))
+    fi
 done < <(git -C "${REPO_ROOT}" ls-files -z)
 
+# Check if any broken symlinks were found
 if [ "${NUM_BROKEN_SYMLINKS}" -gt 0 ]; then
-  fatal "❌ Found ${NUM_BROKEN_SYMLINKS} symlinks."
+    log "❌ Found ${NUM_BROKEN_SYMLINKS} broken symlinks."
+    exit 1
 fi
 
-log "✅ Found 0 symlinks."
+log "✅ Found 0 broken symlinks."

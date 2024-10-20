5909028c583de2b13c59898dc1fd6cc54f76a643
GErP83 <gurrka@gmail.com>
2024-10-20 19:51:07 +0200

    Update run-swift-format.sh

--- scripts/run-swift-format.sh
+++ scripts/run-swift-format.sh
@@ -5,8 +5,7 @@ log() { printf -- "** %s\n" "$*" >&2; }
 error() { printf -- "** ERROR: %s\n" "$*" >&2; }
 fatal() { error "$@"; exit 1; }
 
-CURRENT_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-REPO_ROOT="$(git -C "${CURRENT_SCRIPT_DIR}" rev-parse --show-toplevel)"
+REPO_ROOT="$(git -C "$PWD" rev-parse --show-toplevel)"
 
 FORMAT_COMMAND=(lint --strict)
 for arg in "$@"; do
